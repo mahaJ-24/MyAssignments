be3c41636b431d292cb7203579dbbd769aa19f87
mahaJ-24 <mahalakshmijagadeesan1@gmail.com>
2025-10-12 09:45:53 +0530

    Add files via upload
    adding home assignments

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,33 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			String name = "Mahalakshmi";
+			int age = 36;
+			double	salary = 55000.75;
+			char	gender = 'F';
+			boolean	acceptTerms = true;
+			float	rating = 4.5f;
+			long mobileNumber = 9876543210L;
+			int	pinCode = 600100;
+			byte	luckyNumber = 3;
+			
+			// Lets print the details			
+			System.out.println("Employee Details");			
+			System.out.println("Employee Name: "+name);
+			System.out.println("Employee Age: "+age);
+			System.out.println("Employee Salary: "+salary);
+			System.out.println("Employee Gender: "+gender);
+			System.out.println("Employee Rating: "+rating);
+			System.out.println("Employee MobileNumber: "+mobileNumber);
+			System.out.println("Employee luckyNumber: "+luckyNumber);
+			System.out.println("Employee Pincode: "+pinCode);
+			System.out.println("Has Employee accepted the terms? "+acceptTerms);
+			
+
+	}
+
+}
